9965918ccc337ca3bd0a5a843e04ff87f2f38433
Didier_Defrance <didier@d-defrance.fr>
2025-12-08 15:12:54 +0100

    s

--- tp/decembre2025/tp_bases/main.mjs
+++ tp/decembre2025/tp_bases/main.mjs
@@ -1,5 +1,8 @@
 import calcul, { carre , racineCarree} from './calcul.mjs';
+import { markdown  } from 'markdown' //necessite  npm install -s markdown
 console.log("le carre de 4 vaut " + carre(4));
 console.log("la racine carree de 9 vaut " + racineCarree(9));
 console.log("3+4= " + calcul.add(3,4));
 console.log("calcul.name =" + calcul.name);
+var sHtml = markdown.toHTML("**" + "coucou" + "**");
+console.log(sHtml);
