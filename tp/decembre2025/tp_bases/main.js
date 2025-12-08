7adf0ed391c77bf71d14db31d93594e71e58a09f
Didier_Defrance <didier@d-defrance.fr>
2025-12-08 15:09:42 +0100

    s

--- tp/decembre2025/tp_bases/main.js
+++ tp/decembre2025/tp_bases/main.js
@@ -1,3 +1,6 @@
 var calcul = require('./calcul');
+var markdown  = require('markdown').markdown; //necessite  npm install -s markdown
 console.log("le carre de 4 vaut " + calcul.carre(4));
+var sHtml = markdown.toHTML("**" + "coucou" + "**");
+console.log(sHtml);
 console.log("la racine carree de 9 vaut " + calcul.racineCarree(9));
