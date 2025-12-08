ba1f0c6c9ab32e7f8b3f596e5bf0fc266ca90910
Didier_Defrance <didier@d-defrance.fr>
2025-12-08 13:46:47 +0100

    calcul.js

---
+++ tp/decembre2025/tp_bases/calcul.js
@@ -0,0 +1,10 @@
+function carre(x){
+    return x*x;
+}
+
+function racineCarree(x){
+    return Math.sqrt(x);
+}
+
+module.exports.carre = carre ;
+module.exports.racineCarree = racineCarree ;
---
+++ tp/decembre2025/tp_bases/main.js
@@ -0,0 +1,3 @@
+var calcul = require('./calcul');
+console.log("le carre de 4 vaut " + calcul.carre(4));
+console.log("la racine carree de 9 vaut " + calcul.racineCarree(9));
