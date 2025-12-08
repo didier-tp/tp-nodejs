f0fee93ce9d79e7669cde96ca34f820f81cb3967
Didier_Defrance <didier@d-defrance.fr>
2025-12-08 13:56:57 +0100

    s

---
+++ tp/decembre2025/tp_bases/calcul.mjs
@@ -0,0 +1,16 @@
+export function carre(x){
+    return x*x;
+}
+
+export function racineCarree(x){
+    return Math.sqrt(x);
+}
+
+export function add(x,y){
+    return x+y;
+}
+
+export default {
+    name : 'calcul',
+    add : add
+}
