207d4cccf789d1699b4e654c48b49ff46d8692e5
Didier_Defrance <didier@d-defrance.fr>
2025-12-08 15:17:15 +0100

    s

--- tp/decembre2025/tp_bases/main2.js
+++ tp/decembre2025/tp_bases/main2.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const calcul2_1 = require("./calcul2");
+const markdown_1 = require("markdown"); //necessite  npm install -s markdown
 console.log("le carre de 4 vaut " + (0, calcul2_1.carre)(4));
 console.log("la racine carree de 9 vaut " + (0, calcul2_1.racineCarree)(9));
 console.log("3+4= " + calcul2_1.default.add(3, 4));
 console.log("calcul.name =" + calcul2_1.default.name);
+var sHtml = markdown_1.markdown.toHTML("**" + "coucou" + "**");
+console.log(sHtml);
 //# sourceMappingURL=main2.js.map
